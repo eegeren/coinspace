eb7cee8ca532512927c0226b9d1cf3f35ba790b6
eegeren <yusufege.erenn@gmail.com>
2025-05-24 00:51:52 +0300

    Cleaned and Reset FASTAPI

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
-import os
 from fastapi import FastAPI, Request
+from telegram.ext import ApplicationBuilder
 from telegram import Update
-from bot.telegram_bot import telegram_app  # doğru import
-
+import os, asyncio
 from dotenv import load_dotenv
+
 load_dotenv()
 
+TOKEN = os.getenv("TELEGRAM_TOKEN")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 PORT = int(os.getenv("PORT", 8000))
 
-fastapi_app = FastAPI()
+app = FastAPI()
+tg_app = ApplicationBuilder().token(TOKEN).build()
 
-@fastapi_app.on_event("startup")
-async def on_startup():
-    await telegram_app.bot.set_webhook(WEBHOOK_URL)
+@app.on_event("startup")
+async def startup():
+    await tg_app.bot.set_webhook(WEBHOOK_URL)
 
-@fastapi_app.post("/webhook")
-async def telegram_webhook(request: Request):
-    data = await request.json()
-    update = Update.de_json(data, telegram_app.bot)
-    await telegram_app.update_queue.put(update)
+@app.post("/webhook")
+async def webhook(req: Request):
+    data = await req.json()
+    update = Update.de_json(data, tg_app.bot)
+    await tg_app.update_queue.put(update)
     return {"ok": True}
